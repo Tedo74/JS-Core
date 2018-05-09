22bf0d6d4dd482b6b49579344b9acf4de8898ba1
Tedo74 <teddy2@abv.bg>
2018-05-09 18:03:13 +0300

    removed

--- FigureArea.js
+++
@@ -1,8 +0,0 @@
-function figArea(w, h, W, H) {
-    let fig1 = W * H;
-    let fig2 = w * h;
-    let figToSubtract = Math.min(H, h) * Math.min(W, w);
-
-    let totalArea = fig1 + fig2 - figToSubtract;
-    console.log(totalArea);
-}
--- Sum3Numbers.js
+++
@@ -1,4 +0,0 @@
-function sum3Numbers(a, b, c) {
-    let sumAll = a + b + c;
-    console.log(sumAll);
-}
