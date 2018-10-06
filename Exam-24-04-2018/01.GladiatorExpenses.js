c6cdb36124916e805f815c471a9dca9ebdf2a41e
Tedo74 <teddy2@abv.bg>
2018-10-06 16:13:48 +0300

    edit

--- Exam-24-04-2018/01.GladiatorExpenses.js
+++ Exam-24-04-2018/01.GladiatorExpenses.js
@@ -1,36 +1,9 @@
-function gladiatorExpenses(lostFights, helmetPrice, swordPrice, shieldPrice, armorPrice) {
-    let total = 0;
-    let shieldBrokenCount = 0;
+function gladiatorExpenses(lostFights, helmetPrice, swordPrice, shieldPrice, armorPrice){
+    let helmetCosts = Math.floor(lostFights / 2) * helmetPrice;
+    let swordCosts = Math.floor(lostFights / 3) * swordPrice;
+    let shieldCosts = Math.floor(lostFights / 6) * shieldPrice;
+    let armorCosts = Math.floor(lostFights / 12) * armorPrice;
+    let totalSum = helmetCosts + swordCosts + shieldCosts + armorCosts;
+    console.log(`Gladiator expenses: ${totalSum.toFixed(2)} aureus`);
 
-    for (let i = 1; i <= lostFights; i++) {
-        let isHelmetBroken = false;
-        let isSwordBroken = false;
-
-        if (i % 2 === 0){
-            total += helmetPrice;
-            isHelmetBroken = true;
-        }
-        if (i % 3 === 0){
-            total += swordPrice;
-            isSwordBroken = true;
-        }
-        if (isHelmetBroken && isSwordBroken){// shield is broken
-            total += shieldPrice;
-            shieldBrokenCount++;
-        }
-        if (isHelmetBroken && isSwordBroken && shieldBrokenCount > 0
-            && shieldBrokenCount % 2 === 0){
-            total += armorPrice;
-        }
-    }
-    console.log(`Gladiator expenses: ${total.toFixed(2)} aureus`);
 }
-
-gladiatorExpenses(
-    7,
-    2,
-    3,
-    4,
-    5
-
-);
